e015e02d7629de7c86e3948a25ea5dc0566c44e0
Patrick <patrick.roessler@dci-student.org>
2024-06-28 09:56:42 +0200

    update

--- src/components/Hero.component.jsx
+++ src/components/Hero.component.jsx
@@ -7,11 +7,11 @@ export function Hero() {
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">I'm a Web Devolper</h1>
+            <h1 className="typewriter text-5xl font-bold">I'm a Web Devolper</h1>
             <p className="py-6">
             Dive into creative web projects, innovative solutions, and cutting-edge technologies. Discover my passion for top-notch web development!
             </p>
-            <a className="btn btn-primary text-white">Contact me</a>
+            <a href="#contact" className="btn btn-primary text-white">Contact</a>
           </div>
         </div>
       </div>
